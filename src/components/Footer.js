6030d401ca6ab50834f243e59bdbb82a28245630
kimyeonji <hfgj0014@naver.com>
2024-08-22 17:25:58 +0900

    푸터 완성

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 export const Footer = () => {
   return (
-    <Box maxW="500px" mx="auto">
-      footer
+    <Box
+      maxW="500px"
+      w={"100%"}
+      mx="auto"
+      borderTop={"1px solid #423F3E"}
+      padding={"20px"}
+    >
+      <Text textAlign={"center"} color={"#423F3E"}>
+        &copy; KimYeonJI 2024
+      </Text>
+      <Text textAlign={"center"} color={"#423F3E"}>
+        <a href="https://github.com/kimyeonji713/chewping">
+          https://github.com/kimyeonji713/chewping
+        </a>
+      </Text>
     </Box>
   );
 };
