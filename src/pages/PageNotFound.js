042ab24975fa2420654eb80ea708df08fe9d2bc8
kimyeonji713 <hfgj0014@naver.com>
2024-08-26 14:51:53 +0900

    404 완성

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,3 +1,30 @@
+import { Box, Button, Text, useColorModeValue } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { routes } from "../routes";
+
 export const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  const pointColor = useColorModeValue("#178254", "#FFAD60");
+  const bg = useColorModeValue("#fdfdfd", "#011627");
+
+  return (
+    <Box
+      maxW={"500px"}
+      mx={"auto"}
+      h={"800px"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      bgColor={bg}
+    >
+      <Text textAlign={"center"} fontSize={"35px"} color={pointColor}>
+        Page Not Found
+      </Text>
+      <Button marginTop={"50px"} bg={pointColor}>
+        <Link to={routes.home}>
+          <Text color={"#fff"}>홈으로 돌아가기</Text>
+        </Link>
+      </Button>
+    </Box>
+  );
 };
